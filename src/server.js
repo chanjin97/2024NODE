3b64bfd0e24a4fb60efce2da776bcf06e02471a2
chanjin97 <chanjindark@naver.com>
2024-03-18 17:31:35 +0900

    20240318_node crud_오후

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const User = require("./model/User.js");
+const { User } = require("./model/User.js");
 
 const users = [];
 
 const MONGO_URL =
   "mongodb+srv://chanjindark:Hvn5HPr63NFqywPK@mongodb.kjsgwov.mongodb.net/?retryWrites=true&w=majority&appName=Mongodb";
-
 // let result = mongoose.connect(MONGO_URL);
 // console.log(result);
 
@@ -15,30 +14,57 @@ const MONGO_URL =
 //   return console.log(result);
 // });
 
-// async function fn() {}
-// const fn = async function () {};
+// async function fn(){}
+// const fn = async function(){}
 
-// fn();
+// fn()
 
-const fn = new Promise(function (resolve, reject) {});
+// const fn = new Promise(function (resolve, reject) {});
 
 const server = async function () {
   try {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL); //promis
     console.log("db connected");
     app.use(express.json());
 
-    app.get("/user", function (req, res) {
-      return res.send({ user: users });
+    app.get("/user", async function (req, res) {
+      //1
+      //return res.send({user: users});
+      try {
+        const users = await User.find({});
+        return res.send({ users });
+      } catch (error) {
+        return res.status(500).send({ error: error.message });
+      }
     });
 
-    app.post("/user", function (req, res) {
-      users.push({
-        name: req.body.name,
-        age: req.body.age,
-      });
+    app.post("/user", async function (req, res) {
+      // 01
+      // users.push({
+      //   name: req.body.name,
+      //   age: req.body.age,
+      // });
       // users.push({name: "홍길동", age: 30});
-      return res.send({ success: true });
+      // return res.send({success: true});
+
+      //02
+      // let username = req.body.username
+      // let name = req.body.name
+      try {
+        let { username, name } = req.body;
+        if (!username) {
+          return res.status(400).send({ error: "이름이 없네요!!!!!" });
+        }
+        if (!name || !name.first || !name.last) {
+          return res.status(400).send({ error: "성/이름이 없네요!!!" });
+        }
+
+        const user = new User(req.body);
+        await user.save();
+        res.send({ user });
+      } catch (error) {
+        return res.status(500).send({ error: error.message });
+      }
     });
 
     app.listen(3000);
