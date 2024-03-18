4fbaac28d5dde9199e5288f6436f8bb630f3cc34
chanjin97 <chanjindark@naver.com>
2024-03-18 12:20:40 +0900

    del

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", function (req, res) {
-  return res.send("hello node!!!");
-});
-
-app.listen(3000);
-// http://localhose:3000/
